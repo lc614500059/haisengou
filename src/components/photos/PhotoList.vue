2960f24bc5b4bd8f2a4d2e4f33575a4d2ea77503
lc614500059 <17644200523@163.com>
2017-12-24 09:59:10 +0800

    实现懒加载

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -3,45 +3,19 @@
       <div id="slider" class="mui-slider">
 				<div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
 					<div class="mui-scroll">
-						<a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-							推荐
-						</a>
-						<a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-							热点
-						</a>
-						<a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-							北京
-						</a>
-						<a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-							社会
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							娱乐
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							ddd
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							ccc
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							bbb
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							aaa
-						</a>
+						<span :class="['mui-control-item', item.id===0? 'mui-active':'']" v-for="item in cateList" :key="item.id">
+							{{ item.title }}
+						</span>
 					</div>
 				</div>
-			</div>
 
-			<h3>图片分享组件</h3>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
+			</div>
+	    <ul class="myul">
+          <li v-for="(item,i) in imgList" :key="i">
+            <img v-lazy="item.img_url">
+          </li>
+      </ul>    
     </div>
 </template>
 
@@ -49,25 +23,58 @@
 // 导入mui
 import mui from "../../../lib/mui/js/mui.js";
 
+// import Vue from 'vue'
+
+import { Lazyload } from "mint-ui";
+// Vue.use(Lazyload);
+
 export default {
   data() {
-    return {};
+    return {
+      cateList: [],
+      imgList: []
+    };
   },
-  methods: {},
+  created() {
+    this.getImgCate();
+    this.getImgByCate(0);
+  },
+  methods: {
+    async getImgCate() {
+      const { data } = await this.$http.get("/api/getimgcategory");
+      if (data.status === 0) {
+        data.message.unshift({ title: "全部", id: 0 });
+        this.cateList = data.message;
+      }
+    },
+    async getImgByCate(id) {
+      const { data } = await this.$http.get("/api/getimages/" + id);
+      if (data.status === 0) {
+        this.imgList = data.message;
+      }
+    }
+  },
   mounted() {
     mui(".mui-scroll-wrapper").scroll({
       deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
   }
+  // props:["id"]
 };
 </script>
 
 <style lang="scss" scoped>
-
-.mui-slider{
-	touch-action: pan-x;
+.mui-slider {
+  touch-action: pan-x;
 }
 
-
+.myul {
+  background-color: #000;
+  img[lazy="loading"] {
+    width: 40px;
+    height: 300px;
+    margin: auto;
+  }
+}
 </style>
 
